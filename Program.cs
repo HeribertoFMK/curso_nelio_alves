71d8e50ad43b3ae768e2d23cfb16e6877428a483
Heriberto FMK <hfmk2015@gmail.com>
2021-05-27 10:39:15 -0300

    segunda alteração

--- Program.cs
+++ Program.cs
@@ -8,49 +8,53 @@ namespace curse01
     {
         static void Main(string[] args)
         {
-           info[] hotel = new info[10];
-           
-           System.Console.WriteLine("How Rooms Will be Rented");
-           int n = int.Parse(Console.ReadLine());
+            info[] hotel = new info[10];
 
-              
-           for (int i = 1; i <= n; i++)
-           {
-               System.Console.WriteLine($"Ren #{i}:");
-               System.Console.Write("Name: ");
-               string name = Console.ReadLine();
-               System.Console.Write("Email: ");
-               string email = Console.ReadLine();
-               System.Console.Write("Room: ");
-               int room = int.Parse(Console.ReadLine());
-               hotel[room] = new info(name,email); 
+            System.Console.WriteLine("How Rooms Will be Rented");
+            int n = int.Parse(Console.ReadLine());
 
 
-           }
-           
-           System.Console.WriteLine("Busy Room:");
-           for (int i = 0; i < 10; i++){
-               
-             if(hotel[i]!= null) {
-
-                 System.Console.WriteLine(i+": " +hotel[i]);
-
-
-
-             }                      
-               
-           }
-           
-            
-
-                  
-
-                 
-           
-              
+            for (int i = 1; i <= n; i++)
+            {
+                System.Console.WriteLine($"Ren #{i}:");
+                System.Console.Write("Name: ");
+                string name = Console.ReadLine();
+                System.Console.Write("Email: ");
+                string email = Console.ReadLine();
+                System.Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                hotel[room] = new info(name, email);
 
 
-           
+            }
+
+            System.Console.WriteLine("Busy Room:");
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (hotel[i] != null)
+                {
+
+                    System.Console.WriteLine(i + ": " + hotel[i]);
+
+                    System.Console.WriteLine("hello");
+
+
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
